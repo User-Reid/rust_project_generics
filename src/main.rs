a8777ae85abc572c185c4320729a8c3069ca6dc4
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-12 21:25:14 -0600

    successfully completed the project. I ran into a little trouble when it weas asking for the T type to be DigitalContent. I of course knew that i could have simply just done what it asked, but i was trying to make it work with the type T generic and it wasnt working for me. I will have to discover why...

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+enum DigitalContent {
+    AudioFile,
+    VideoFile
+}
+
+#[derive(Debug)]
+struct ChatMessage<T> {
+    content: T,
+    time: String,
+}
+
+impl ChatMessage<DigitalContent> {
+    fn consume_entertainment(&self) {
+        println!("Watching the {:?}", self.content)
+    }
+}
+
+impl<T> ChatMessage<T> {
+    fn retrieve_time(self) -> String {
+        self.time.clone()
+    } 
+}
+
+fn main() {
+    let example: ChatMessage<DigitalContent> = ChatMessage {
+        content: DigitalContent::AudioFile,
+        time: String::from("9:00PM"),
+    };
+    let alpha: ChatMessage<&str> = ChatMessage {
+        content: "Howdy",
+        time: String::from("8:45PM")
+    };
+    let beta: ChatMessage<String> = ChatMessage {
+        content: String::from("What you doin tonight?"),
+        time: String::from("9:03PM"),
+    };
+
+    example.consume_entertainment();
+    println!("{:?}", example.retrieve_time());
+    println!("{:?}", alpha.retrieve_time());
+    println!("{:?}", beta.retrieve_time());
+}
